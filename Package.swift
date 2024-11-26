22efc47072db8e36d0bb29900317f9cfa2a93675
Omar Albeik <omaralbeik@gmail.com>
2024-11-26 18:11:32 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
     ),
     .binaryTarget(
       name: "VLCKit",
-      url: "https://github.com/omaralbeik/VLC/releases/download/3.6.2/VLCKit.zip",
+      url: "https://github.com/omaralbeik/VLC/releases/download/3.6.3/VLCKit.zip",
       checksum: "fae5c81f564ee89a682b3af2d063a2b3e20a0991536a2e2c3197fd8b5ddf6f88"
     ),
   ]
